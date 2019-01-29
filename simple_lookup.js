d0c4ff6f0d3b334a2fb57a655365859871624f94
Tony Kieling <tony.kieling@gmail.com>
2019-01-29 21:13:03 +0000

    second commit

---
+++ simple_lookup.js
@@ -0,0 +1,57 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const [name] = process.argv.slice(2);
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect();
+
+function convertToDate(data) {
+  const birthday = new Date(data);
+  const year = birthday.getFullYear();
+  const month = (birthday.getMonth() < 10)? ("0" + (birthday.getMonth() + 1)) : (birthday.getMonth() + 1);
+  const day = (birthday.getDate() < 10)? ("0" + (birthday.getDate())) : (birthday.getDate());
+  return(`${year}-${month}-${day}`);
+}
+
+function doQuery(client, query, values) {
+  client.query(query, values, (err, res) => {
+    if (err) {
+      console.log(err);
+      return false;
+    }
+
+    if (res.rows.length) {
+      console.log(`Found ${res.rows.length} person(s) by the name ${values}:`);
+      let count = 1;
+      res.rows.forEach(famous_people => {
+        const birthday = convertToDate(famous_people.birthdate);        
+        console.log(`- ${count}: ${famous_people.first_name} ${famous_people.last_name}, born '${birthday}'`);    
+        count += 1;
+      });
+
+    } else {
+      console.log("No results found");
+    }
+    client.end();
+  });
+}
+
+function findFamous(client, data) {
+  const query = "SELECT * FROM famous_people WHERE first_name=$1 OR last_name=$1";
+  const values = [data];
+
+  doQuery(client, query, values);
+}
+
+
+console.log('Searching...');
+findFamous(client, name);
