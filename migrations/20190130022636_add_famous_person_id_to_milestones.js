2ca228bd7a3fecd8d064145211ebd59d452d62ae
Tony Kieling <tony.kieling@gmail.com>
2019-01-30 02:36:05 +0000

    assigment's done

---
+++ migrations/20190130022636_add_famous_person_id_to_milestones.js
@@ -0,0 +1,20 @@
+exports.up = function(knex, Promise) {  
+  return Promise.all([
+    knex.schema.table('milestones', function(table){
+
+      table.integer('famous_person_id').unsigned().notNullable();
+    
+      table.foreign('famous_person_id').references('id').inTable('famous_people');
+
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {  
+  return Promise.all([
+    knex.schema.table('milestones', function(table){
+      table.dropColumn('famous_person_id');
+    })
+  ])
+};
+
